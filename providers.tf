a354d7818c35e5a3fc566406ea5bd64142b7fd5d
Test5532 <test5532>
2025-03-18 15:55:50 +0530

    Adding TF lock file and Ignore .terraform file

---
+++ providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+// = (Equal to) → Specifies an exact version (e.g., = 5.91.0 means only that version will be used).
+// ~> (Pessimistic Constraint Operator) → Allows patch and minor updates but restricts major versions like 5.91.1 or 5.92.0 but not 6.0.
+
+provider "aws" {
+    region = "us-east-1"
+    access_key = "my-access-key"
+    secret_key = "my-secret-key"
+}
