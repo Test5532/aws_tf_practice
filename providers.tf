0e14fb23d5de1696107ffec5aae71aa764f9a11e
Test5532 <test5532>
2025-03-19 09:22:47 +0530

    seperated credentials

--- providers.tf
+++ providers.tf
@@ -10,8 +10,3 @@ terraform {
 // = (Equal to) → Specifies an exact version (e.g., = 5.91.0 means only that version will be used).
 // ~> (Pessimistic Constraint Operator) → Allows patch and minor updates but restricts major versions like 5.91.1 or 5.92.0 but not 6.0.
 
-provider "aws" {
-    region = "us-east-1"
-    access_key = "AKIATDSL7OC4EDB6OAPR"
-    secret_key = "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"
-}
